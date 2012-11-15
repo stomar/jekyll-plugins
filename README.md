faac80d9931b8fb20d35f9f04a880abba6ce4f49
Marcus Stollsteimer <sto.mar@web.de>
2012-11-15 21:13:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+Jekyll plugins
+==============
+
+file_link tag
+-------------
+
+A tag that generates a link (in markdown format) to a local file
+with its basename as link text. When the file can not be found
+the tag only generates the basename without a link.
+
+Usage:
+
+    {% file_link path/to/file %}  -> [file](path/to/file)
+    {% file_link /path/to/file %} -> [file](/path/to/file)
+
+The path can be given relative (to the current page)
+or absolute, with the Jekyll source directory as root.
+
+Installation
+------------
+
+Copy the Ruby files (`*.rb`) into the `_plugins` directory.
+
+License
+-------
+
+Copyright &copy; 2012 Marcus Stollsteimer
+
+This is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 3 or later (GPLv3+),
+see [www.gnu.org/licenses/gpl.html](http://www.gnu.org/licenses/gpl.html).
+There is NO WARRANTY, to the extent permitted by law.
+
---
+++ file_link.rb
@@ -0,0 +1,57 @@
+module Jekyll
+
+  # A tag that generates a link (in markdown format) to a local file
+  # with its basename as link text. When the file can not be found
+  # the tag only generates the basename without a link.
+  #
+  # Usage:
+  #
+  #     {% file_link path/to/file %}  -> [file](path/to/file)
+  #     {% file_link /path/to/file %} -> [file](/path/to/file)
+  #
+  # The path can be given relative (to the current page)
+  # or absolute, with the Jekyll source directory as root.
+  #
+  # Installation:
+  #
+  # Copy this file into the +_plugins+ directory.
+  #
+  # Copyright (C) 2012 Marcus Stollsteimer
+  #
+  # License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
+  class FileLinkTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @path = text.strip
+      @basename = File.basename(@path)
+    end
+
+    def render(context)
+      page = context.environments.first['page']
+      page_dir = File.dirname(page['url'])
+
+      # create path relative to site root
+      path = path_relative? ? File.join(page_dir, @path) : @path
+      path.gsub!(%r{\A/}, '')
+
+      if File.exist?(path)
+        out = "[#{@basename}](#{@path})"
+        #out = "<a href='#{@path}'>#{@basename}</a>"
+      else
+        out = @basename
+        warn "file_link plugin: File not found: `#{path}'"
+      end
+
+      out
+    end
+
+    private
+
+    def path_relative?
+      !(%r{\A/} =~ @path)
+    end
+  end
+end
+
+Liquid::Template.register_tag('file_link', Jekyll::FileLinkTag)
